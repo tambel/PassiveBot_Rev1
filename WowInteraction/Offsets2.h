f4f062e232c6847895fbcedc01dac20ba190a57f
tambel <arttambel@gmail.com>
2016-10-18 15:28:28 +0400

    offset2

--- WowInteraction/Offsets2.h
+++ WowInteraction/Offsets2.h
@@ -3,9 +3,9 @@ namespace WowOffsets2
 {
     enum Client
     {
-        Connecting=0xF3A200,
-        LoggedIn=0xECA9C0,
-        CharSelecting=0xF3A4FC
+        Connecting = 0xF3A200, // 15966720
+        LoggedIn = 0xECA9C0, // 15509952
+        CharSelecting = 0xF3A4FC // 15967484
     };
 
 }
