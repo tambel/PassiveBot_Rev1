5c69d67077c5cdc1b13ab324b973f02a06016e42
tambel <arttambel@gmail.com>
2016-10-26 16:36:08 +0400

    before ida update

--- BotInteractor/stdafx.h
+++ BotInteractor/stdafx.h
@@ -9,8 +9,18 @@
 #include <memory>
 #include <fstream>
 #include <iostream>
+
+#define TINS_HAVE_TCPIP
 #include "tins\tins.h"
 #include "tins\network_interface.h"
+#include "tins/tcp_ip/stream_follower.h"
+#include "tins/sniffer.h"
+#include "tins/packet.h"
+#include "tins/ip_address.h"
+#include "tins/ipv6_address.h"
+
+#include "tins/network_interface.h"
+#include <tins/exceptions.h>
 #include "Tools\Tools.h"
 #include "WowInteraction\WowInteraction.h"
 #include "WowWorldsData\WowWorldsData.h"
