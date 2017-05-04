da4e4c9aa6cc986fc663106e3600c992ae19ca5b
Arthur Kamalov <arttambel@gmail.com>
2017-05-04 13:33:44 +0400

    revert

--- WorldViewer/WorldViewer.cpp
+++ WorldViewer/WorldViewer.cpp
@@ -32,12 +32,12 @@ int main()
 		{
 			cout << i << ": " << j << endl;
 			tc++;
-			//gnm.AddTile(Point2DI(i, j));
+			///gnm.AddTile(Point2DI(i, j));
 			
 		}
 	//gnm.Save();
 	cout << tc<<endl;
-	gnm.Load();
+	//gnm.Load();
 
 	
 	
@@ -65,39 +65,11 @@ int main()
 	auto barrens = *find_if(areas.begin(), areas.end(), [](auto & a) {return a.name == "Barrens"; });
 	auto bb = mulgore.GetBoundingBox();
 
-	auto is_intersect = [](BoundingBox & b1, BoundingBox & b2)
-	{
-		if (b1.IsInside2D(b2.up) || b1.IsInside2D(Vector3(b2.up.x, b2.down.y, 0)) || (b1.IsInside2D(b2.down)) || b1.IsInside2D(Vector3(b2.down.x, b2.up.y, 0)))
-		{
-			return true;
-		}
-		return false;
-	};
-	vector<WorldMapArea> neigh;
-	for (auto & a : areas)
-	{
-		if (a.name != mulgore.name)
-		{
-			if (is_intersect(mulgore.GetBoundingBox(), a.GetBoundingBox()))
-			{
-				neigh.push_back(a);
-			}
-		}
-	}
-
-
-
 	
-	
-	gnm.FindPath(Vector3(-2435.32, -530.151,-8.99781), Vector3(-1344.39,195.327,61.0569));
-	auto v1 = gnm.GetLastPath();
-	viewer.GetState()->AddLineStrip(v1);
+	//gnm.FindPath(Vector3(-2435.32, -530.151,-8.99781), Vector3(-1344.39,195.327,61.0569));
+	//auto v1 = gnm.GetLastPath();
+	//viewer.GetState()->AddLineStrip(v1);
 
-	float w = abs(mulgore.right-mulgore.left)/256;
-	float h = abs(mulgore.bottom - mulgore.top)/256;
-	cout << w << " " << h << endl;
-	double sq = w*h;
-	cout << sq << endl;
 
 	for (auto & a : areas)
 	{
